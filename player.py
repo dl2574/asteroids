3e0268548290b43ab144fd876096735d1819237b
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-04-06 15:52:51 +0300

    Rate limit how often the player can shoot

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -28,6 +29,7 @@ class Player(CircleShape):
         velocity = pygame.Vector2(0,1).rotate(self.rotation)
         velocity *= PLAYER_SHOOT_SPEED
         shot.velocity = velocity
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
     
     def move(self, dt):
@@ -36,6 +38,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -46,4 +49,5 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer <= 0:
+                self.shoot()
